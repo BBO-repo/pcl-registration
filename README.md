89cfd250f04f273052a18b071e8d98b173302c90
BBO <bdz_brahim@yahoo.fr>
2024-06-03 22:53:32 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# PCL ICP
+Standalone executable performing PCL Normal Distributions Transform and GICP registrations
+
+### Build and run
+```
+git clone pcl-registration
+cd pcl-registration
+mkdir build && cd build && cmake .. && make
+./align  ../data/16961.280000000.pcd ../data/16961.804000000.pcd
+--- pcl::GICP ---
+single alignment taken time: 53[ms]
+fitness: 0.0455552
+
+--- pcl::NDT ---
+single alignment taken time: 43[ms]
+fitness: 0.0714274
+```
---
+++ src/align.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <chrono>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/registration/ndt.h>
+#include <pcl/registration/gicp.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+// align point clouds and measure processing time
+pcl::PointCloud<pcl::PointXYZ>::Ptr align(pcl::Registration<pcl::PointXYZ, pcl::PointXYZ>::Ptr registration, const pcl::PointCloud<pcl::PointXYZ>::Ptr& target_cloud, const pcl::PointCloud<pcl::PointXYZ>::Ptr& source_cloud ) {
+  registration->setInputTarget(target_cloud);
+  registration->setInputSource(source_cloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr aligned(new pcl::PointCloud<pcl::PointXYZ>());
+
+  std::chrono::steady_clock::time_point tic = std::chrono::steady_clock::now();
+  registration->align(*aligned);
+  std::chrono::steady_clock::time_point toc = std::chrono::steady_clock::now();
+  std::cout << "single alignment taken time: " << std::chrono::duration_cast<std::chrono::milliseconds>(toc-tic).count() << "[ms]" << std::endl;
+  std::cout << "fitness: " << registration->getFitnessScore() << std::endl << std::endl;
+
+  return aligned;
+}
+
+
+int main(int argc, char** argv) {
+  if(argc != 3) {
+    std::cout << "usage: align target.pcd source.pcd" << std::endl;
+    return 0;
+  }
+
+  std::string target_pcd = argv[1];
+  std::string source_pcd = argv[2];
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+
+  if(pcl::io::loadPCDFile(target_pcd, *target_cloud)) {
+    std::cerr << "failed to load " << target_pcd << std::endl;
+    return 0;
+  }
+  if(pcl::io::loadPCDFile(source_pcd, *source_cloud)) {
+    std::cerr << "failed to load " << source_pcd << std::endl;
+    return 0;
+  }
+
+  // downsampling
+  pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled(new pcl::PointCloud<pcl::PointXYZ>());
+
+  pcl::VoxelGrid<pcl::PointXYZ> voxelgrid;
+  voxelgrid.setLeafSize(0.1f, 0.1f, 0.1f);
+
+  voxelgrid.setInputCloud(target_cloud);
+  voxelgrid.filter(*downsampled);
+  *target_cloud = *downsampled;
+
+  voxelgrid.setInputCloud(source_cloud);
+  voxelgrid.filter(*downsampled);
+  source_cloud = downsampled;
+
+  // benchmark
+  std::cout << "--- pcl::GICP ---" << std::endl;
+  pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::Ptr gicp(new pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr aligned = align(gicp, target_cloud, source_cloud);
+
+  std::cout << "--- pcl::NDT ---" << std::endl;
+  pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>::Ptr ndt(new pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>());
+  ndt->setResolution(1.0);
+  aligned = align(ndt, target_cloud, source_cloud);
+
+  // visualization
+  pcl::visualization::PCLVisualizer vis("vis");
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> target_handler(target_cloud, 255.0, 0.0, 0.0);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_handler(source_cloud, 0.0, 255.0, 0.0);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> aligned_handler(aligned, 0.0, 0.0, 255.0);
+  vis.addPointCloud(target_cloud, target_handler, "target");
+  vis.addPointCloud(source_cloud, source_handler, "source");
+  vis.addPointCloud(aligned, aligned_handler, "aligned");
+  vis.spin();
+
+  return 0;
+}
